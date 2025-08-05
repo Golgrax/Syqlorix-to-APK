98c54633644ef7517e70359d2ff39a67be10899e
Karl Benjamin Bughaw <pepe.kotelurow@gmail.com>
2025-08-05 09:43:36 +0000

    .

--- README.md
+++
@@ -1,1 +0,0 @@
-# Syqlorix-to-APK
--- assets/sw.js
+++
@@ -1,7 +0,0 @@
-const CACHE = 's2a-v1';
-self.addEventListener('install', e => e.waitUntil(
-  caches.open(CACHE).then(c => c.addAll(['/', 'assets/py2apk.sh']))
-));
-self.addEventListener('fetch', e => e.respondWith(
-  caches.match(e.request).then(r => r || fetch(e.request))
-));
